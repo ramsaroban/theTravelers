aadc7471361c3b58771cdc4f24cf6afa92e976f3
smartdeven <devendra@smarttech.com.np>
2020-09-29 07:50:28 +0545

    shitenoise config remove

--- travelersdotcom/travelersdotcom/settings.py
+++ travelersdotcom/travelersdotcom/settings.py
@@ -167,7 +167,7 @@ EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
 
 
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 
 # import django_heroku
