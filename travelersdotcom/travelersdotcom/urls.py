7a467e81682136d79c5bd9e95b0f00e5529520ae
ramsaroban <ramsaroban@gmail.com>
2020-10-01 00:42:30 +0530

    static added

--- travelersdotcom/travelersdotcom/urls.py
+++ travelersdotcom/travelersdotcom/urls.py
@@ -40,8 +40,8 @@ urlpatterns = [
     path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
     path('api/auth/', include('travelersUsers.urls')),
-    path('api/profiles/', include('travelersProfiles.urls'))
+    path('api/profiles/', include('travelersProfiles.urls')),
 
-    path('media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
-    path('static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
+    #path('media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
+    #path('static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
 ]
